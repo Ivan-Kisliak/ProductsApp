f5efc36af2ee011f22252cd3fcafe224716939a8
Ivan <ivangodno86@gmail.com>
2024-11-17 13:17:52 +0300

    Add project

---
+++ ProductsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductsApp
+//
+//  Created by  Ivan Kiskyak on 17.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
